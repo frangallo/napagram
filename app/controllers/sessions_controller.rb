76b7aef3acae3a3e023a269e9e7df7688367e73c
frangallo <fgallo@me.com>
2015-10-23 16:03:04 -0700

    finished session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,25 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.find_by_credentials(params[:user][:username],
+                                    params[:user][:password])
+
+    if user
+      login_user!(user)
+      render :text => "Success"
+    else
+      flash.now[:errors] = ["Invalid Credentials"]
+      render :new
+    end
+  end
+
+  def destroy
+    @user = current_user
+    logout_user!
+    render json: @user
+  end
+
 end
