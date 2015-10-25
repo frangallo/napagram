85f65af55135061a079cea491c9aca1e26a77ef5
frangallo <fgallo@me.com>
2015-10-24 21:30:03 -0700

    added user#show

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,12 @@
 class Api::UsersController < ApplicationController
   def index
-    @users = Users.all
+    @users = User.all
     render :index
   end
-  
+
+  def show
+    @user = User.includes(:posts).find(params[:id])
+    render :show
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     get 'sale_items/search', to: 'sale_items#search'
-    resources :comments only: [:show, :index, :create, :destroy]
-    resources :followers only: [:show, :index, :create, :destroy]
-    resources :media only: [:show, :index, :create, :destroy, :update]
-    resources :likes only: [:show, :index, :create, :destroy]
-    resources :pictures only: [:show, :index, :create, :destroy]
+    resources :comments, only: [:show, :index, :create, :destroy]
+    resources :followers, only: [:show, :index, :create, :destroy]
+    resources :media, only: [:show, :index, :create, :destroy, :update]
+    resources :likes, only: [:show, :index, :create, :destroy]
+    resources :pictures, only: [:show, :index, :create, :destroy]
     resources :users, only: [:show, :index]
   end
 end
