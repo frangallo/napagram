6ae6427c84aca2f8edbff975864c23f2594061c7
frangallo <fgallo@me.com>
2015-10-24 18:27:48 -0700

    build seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,28 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 names = ["fran", "greg", "steve", "kerry", "doug", "larry", "amanda", "lauren", "laura", "megan"]
+ids = (1..10).to_a
+10.times do |x|
+  User.create({username: names[x],
+              session_token: names[x],
+              password_digest: names[x],
+              quote: names[x]})
+  Media.create({description:names[x]},
+                author_id: ids.sample])
+
+end
 
 10.times do |x|
-  User.create[{username:names[x], }]
+  Like.create({media_id: ids.sample, user_id: ids.sample})
+end
+
+25.times do |x|
+  rand1 = rand(1..10)
+  rand2 = rand(1..10)
+  until rand2 != rand1
+    rand2 = rand(1..10)
+  end
+
+  Follower.create({follower_id: rand1, followee_id: rand2})
+  Comment.new({body: "Hello#{x}", author_id: rand1, media_id: rand2 })
+end
