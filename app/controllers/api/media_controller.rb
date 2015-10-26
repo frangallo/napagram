0cf1c07d5dda2d1f8f744f0c456cb28ccc4ad3fb
frangallo <fgallo@me.com>
2015-10-25 17:53:22 -0700

    finished media #create, #update

--- app/controllers/api/media_controller.rb
+++ app/controllers/api/media_controller.rb
@@ -1,11 +1,25 @@
 class Api::MediaController < ApplicationController
   def create
+    @post = Medium.new(media_params)
+    @post.author_id = current_user.id
+    if @post.save
+      render json: @post
+    else
+      render json: @post.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   def update
+    @post = Medium.find(params[:id])
+    if @post.update(media_params)
+      render json: @post
+    else
+      render json: @post.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @post = Medium.find(params[:id])
   end
 
   def index
@@ -13,5 +27,11 @@ class Api::MediaController < ApplicationController
 
   def show
   end
-  
+
+  private
+
+  def media_params
+    params.require(:media).permit(:location, :description)
+  end
+
 end
