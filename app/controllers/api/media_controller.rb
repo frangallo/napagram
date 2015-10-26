d7ac9d892c9eafb2cc7c5e38405d64e07cad66b4
frangallo <fgallo@me.com>
2015-10-25 17:41:24 -0700

    finished pictures controller

--- app/controllers/api/media_controller.rb
+++ app/controllers/api/media_controller.rb
@@ -1,2 +1,17 @@
 class Api::MediaController < ApplicationController
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def show
+  end
+  
 end
--- app/controllers/api/pictures_controller.rb
+++ app/controllers/api/pictures_controller.rb
@@ -1,2 +1,26 @@
 class Api::PicturesController < ApplicationController
+  def create
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      render json: @picture
+    else
+      render json: {message: 'failure'}, status: 422
+    end
+  end
+
+  def show
+    @picture= Picture.find(params[:id])
+    render json: @picture
+  end
+
+  def destroy
+    @picture= Picture.find(params[:id])
+    @picture.destroy!
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:url, :thumb_url, :imageable_id, :imageable_type)
+  end
 end
