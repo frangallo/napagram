f516c2b5eac8b58c9ba0a3c04b50a8b12ca06c3e
frangallo <fgallo@me.com>
2015-10-27 14:47:51 -0700

    add follower parse to user model

--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -1,65 +1,77 @@
 Napagram.Models.User = Backbone.Model.extend({
-  urlRoot: '/api/users/',
+  _.extend({}, LikesDemo.Mixins.Likable, {
+    urlRoot: "api/users",
 
-  comments: function () {
-    if (!this._comments) {
-      this._comments = new Napagram.Collections.Comments([], { author: this });
+    followableOptions: {
+      foreignKey: "followee_id"
+    },
+
+    comments: function () {
+      if (!this._comments) {
+        this._comments = new Napagram.Collections.Comments([], { author: this });
+      }
+
+      return this._comments;
+    },
+
+    posts: function (){
+      if (!this._posts) {
+        this._posts = new Napagram.Collections.Posts([], { user: this });
+      }
+
+      return this._posts;
+    },
+
+    //could be an issue
+    picture: function (){
+      if (!this._picture) {
+        this._picture = new Napagram.Models.Pictures([], { user: this });
+      }
+
+      return this._picture;
+    },
+
+    followers: function () {
+      if (!this._followers) {
+        this._followers = new Napagram.Collections.Followers([], { user: this });
+      }
+
+      return this._followers;
+    },
+
+    following: function () {
+      if (!this._following) {
+        this._following = new Napagram.Collections.Following([], { user: this });
+      }
+
+      return this._following;
+    },
+
+    parse: function (response) {
+      if (response.comments) {
+        this.comments().set(response.comments, { parse: true });
+        delete response.comments;
+      }
+      if (response.posts) {
+        this.posts().set(response.posts, { parse: true });
+        delete response.posts;
+      }
+      if (response.followers) {
+        this.followers().set(response.followers, { parse: true });
+        delete response.followers;
+      }
+      if (response.following) {
+        this.following().set(response.following, { parse: true });
+        delete response.following;
+      }
+      if (response.follow) {
+        this.follow().set(response.follow, { parse: true });
+        delete response.follow;
+      }
+
+      return response;
     }
+  })
 
-    return this._comments;
-  },
 
-  posts: function (){
-    if (!this._posts) {
-      this._posts = new Napagram.Collections.Posts([], { user: this });
-    }
-
-    return this._posts;
-  },
-
-  //could be an issue
-  picture: function (){
-    if (!this._picture) {
-      this._picture = new Napagram.Models.Pictures([], { user: this });
-    }
-
-    return this._picture;
-  },
-
-  followers: function () {
-    if (!this._followers) {
-      this._followers = new Napagram.Collections.Followers([], { user: this });
-    }
-
-    return this._followers;
-  },
-
-  following: function () {
-    if (!this._following) {
-      this._following = new Napagram.Collections.Following([], { user: this });
-    }
-
-    return this._following;
-  },
-
-  parse: function (response) {
-    if (response.comments) {
-      this.comments().set(response.comments, { parse: true });
-      delete response.comments;
-    }
-    if (response.posts) {
-      this.posts().set(response.posts, { parse: true });
-      delete response.posts;
-    }
-    if (response.followers) {
-      this.followers().set(response.followers, { parse: true });
-      delete response.followers;
-    }
-    if (response.following) {
-      this.following().set(response.following, { parse: true });
-      delete response.following;
-    }
-
-    return response;
-  }
 });
