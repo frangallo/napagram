1bf62d55f329b29215f529531892404394c83f7f
frangallo <fgallo@me.com>
2015-10-28 11:04:03 -0700

    add comments collection

---
+++ app/assets/javascripts/collections/comments.js
@@ -0,0 +1,21 @@
+Napagram.Collections.Comments = Backbone.Collection.extend({
+  url: 'api/commments',
+  model: Napagram.Models.commment,
+
+  getOrFetch: function(id){
+    var collection = this;
+    var comment = collection.get(id);
+
+    if (!comment){
+      comment = new Napagram.Models.Comment({ id: id });
+      collection.add(comment);
+      user.fetch({
+        error: function(){ collection.remove(comment); }
+      })
+    } else {
+      comment.fetch();
+    }
+    return comment;
+  },
+
+});
