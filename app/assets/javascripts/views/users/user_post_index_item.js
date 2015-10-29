8ac7866a22fbe5e08f3ae6bf1abbe00d2425b3f2
frangallo <fgallo@me.com>
2015-10-28 17:10:22 -0700

    add user post template

---
+++ app/assets/javascripts/views/users/user_post_index_item.js
@@ -0,0 +1,15 @@
+Napagram.Views.UserPostIndexItem = Backbone.View.extend({
+  template: JST["users/user_post_index_item"],
+
+  initialize: function(){
+    this.listenTo(this.model, "sync", this.render);
+  },
+
+  render: function(){
+    this.$el.html(this.template({post: this.model,
+                                 total_comments: this.model.comments().length}));
+    return this;
+  },
+
+
+});
--- app/assets/javascripts/views/users/user_show.js
+++ app/assets/javascripts/views/users/user_show.js
@@ -2,36 +2,38 @@ Napagram.Views.UserShow = Backbone.CompositeView.extend({
   template: JST["users/user_show"],
 
   initialize: function(){
-    // this.renderPosts();
-    // var posts = this.model.posts();
+    var posts = this.model.posts();
+    posts.each(function(post){
+      this.addUserPostIndexItemView(post)
+    })
     this.listenTo(this.model, "sync", this.render);
-    // this.listenTo(posts, "add", this.addUserPostIndexItemView);
-    // this.listenTo(posts, "remove", this.removeUserPostIndexItemView);
-    // this.collection.each(this.addUserPostIndexItemView.bind(this));
+    this.listenTo(posts, "add", this.addUserPostIndexItemView);
+    this.listenTo(posts, "remove", this.removeUserPostIndexItemView);
   },
 
   render: function(){
     this.$el.html(this.template({user: this.model}));
-    // this.attachSubviews();
+    this.attachSubviews();
     return this;
   },
 
   // renderPosts: function(){
+  //   debugger
   //   var self = this;
   //   this.posts.each(function(post){
   //     self.addUserPostIndexItemView(post)
   //   })
   // },
-  //
-  // addUserPostIndexItemView: function(postIndexItem){
-  //   var subview = new Napagram.Views.UserPostIndexItem({
-  //     model: postIndexItem
-  //   });
-  //   this.addSubview('.user-posts', subview);
-  // },
-  //
-  // removeUserPostIndexItemView: function(postIndexItem){
-  //   this.removeModelSubview('.user-post', postIndexItem);
-  // },
+
+  addUserPostIndexItemView: function(postIndexItem){
+    var subview = new Napagram.Views.UserPostIndexItem({
+      model: postIndexItem
+    });
+    this.addSubview('.user-posts', subview);
+  },
+
+  removeUserPostIndexItemView: function(postIndexItem){
+    this.removeModelSubview('.user-post', postIndexItem);
+  },
 
 });
