1347e83a540f3390e2367bd704adce46fc160ebc
frangallo <fgallo@me.com>
2015-10-30 15:36:35 -0700

    add header to backbone initialize

--- app/assets/javascripts/napagram.js
+++ app/assets/javascripts/napagram.js
@@ -4,18 +4,27 @@ window.Napagram = {
   Views: {},
   Routers: {},
   Mixins: {},
-  
+
   initialize: function() {
 
     console.log("hello from backbone")
 
     var feedCollection = new Napagram.Collections.UserFeed();
     feedCollection.fetch();
+    var usersCollection = new Napagram.Collections.Users();
+    var usersCollection.fetch();
 
     var router = new Napagram.Routers.Router({
       $rootEl: $("#content"),
       userFeed: feedCollection
     });
+
+    var header = new Napagram.Views.Header({
+      router: router,
+      collection: userCollection
+    });
+
+    $("#header").html(header.render().$el);
 
     Backbone.history.start();
   }
